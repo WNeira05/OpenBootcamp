caf8ee77242830376e5f941452a07b40f2c2a37e
WNeira05 <walterneira1986@gmail.com>
2022-09-03 15:53:59 -0300

    Entrega ejercicios tema 1

---
+++ src/ejerciciosTema1/TipoDeDatos.java
@@ -0,0 +1,38 @@
+/*
+    -Crea un proyecto de Java desde 0.
+
+    -Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+    -Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+    */
+
+
+
+package ejerciciosTema1;
+
+public class TipoDeDatos {
+
+    public static void main(String[] args) {
+
+        int numberInt = 2567;
+        long numberLong = -92124;
+        double numberDouble = 41246367;
+        float numberFloat = 3.14f;
+        boolean verdadero = true;
+        char letra = 'w';
+        String HelloWorld = "Hola Mundo";
+
+
+        System.out.println("Numero entero: "+ numberInt);
+        System.out.println("Numero long: "+ numberLong);
+        System.out.println("Numero Double: "+ numberDouble);
+        System.out.println("Numero float: "+ numberFloat);
+        System.out.println("Boolean: "+ verdadero);
+        System.out.println("Letra: "+ letra);
+        System.out.println("Cadena de caracteres: "+ HelloWorld);
+
+
+    }
+
+
+}
